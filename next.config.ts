1d58741d7771c17d6809555dc1f3ff67e395b7df
Thien Do <5953369+thien-do@users.noreply.github.com>
2025-04-05 23:03:01 +0700

    Add GFM support

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
 import createMDX from '@next/mdx'
- 
+
 const base: NextConfig = {
   pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
 };
 
-const withMDX = createMDX({})(base)
- 
+const withMDX = createMDX({
+  options: {
+    // @ts-expect-error This is the right way to use with turbopack
+    // https://nextjs.org/docs/app/building-your-application/configuring/mdx#using-plugins-with-turbopack
+    remarkPlugins: [["remark-gfm"]],
+  }
+})(base)
+
 export default withMDX
