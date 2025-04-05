82ae20d3ce35706b29f8b01d232d5b74cbd09be5
Thien Do <5953369+thien-do@users.noreply.github.com>
2025-04-05 23:12:12 +0700

    Add user support

--- src/release/kit/user.ts
+++
@@ -1,3 +0,0 @@
-export const RELEASE_USER = {
-  "chinh": "https://www.linkedin.com/in/chinhq"
-}
---
+++ src/release/kit/user.tsx
@@ -0,0 +1,22 @@
+import { ReactElement } from "react"
+
+const URLS = {
+  chinh: "https://www.linkedin.com/in/chinhq",
+  hai: "https://github.com/ng-hai",
+}
+
+export function ReleaseUser(props: {
+  name: keyof typeof URLS
+}): ReactElement {
+  const { name } = props
+
+  const url = URLS[name]
+
+  // This should not happen type-wise,
+  // but since this component is used in markdown,
+  // we do need to handle this case.
+  if (url === undefined)
+    throw new Error(`Unknown user: ${name}`)
+
+  return <a target="_blank" href={url}>@{name}</a>
+}
