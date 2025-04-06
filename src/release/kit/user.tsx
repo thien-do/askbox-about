a5b37eab8d810a5b852613a085c3f7598a498639
Thien Do <5953369+thien-do@users.noreply.github.com>
2025-04-07 00:17:48 +0700

    fix

--- src/release/kit/user.tsx
+++ src/release/kit/user.tsx
@@ -1,26 +1,26 @@
 import { ReactElement } from "react"
 
 const URLS = {
-	chinh: "https://www.linkedin.com/in/chinhnq",
-	hai: "https://github.com/ng-hai",
-	"Nhi Ly": "https://github.com/NgocNhi123",
-	"huy": "https://github.com/rknguyen",
-	"khoa": "https://github.com/ldakhoa",
-	"monody": "https://github.com/monodyle"
+  chinh: "https://www.linkedin.com/in/chinhnq",
+  hai: "https://github.com/ng-hai",
+  nhi: "https://github.com/NgocNhi123",
+  huy: "https://github.com/huyng12",
+  khoa: "https://github.com/ldakhoa",
+  minh: "https://github.com/monodyle"
 }
 
 export function ReleaseUser(props: {
-	name: keyof typeof URLS
+  name: keyof typeof URLS
 }): ReactElement {
-	const { name } = props
+  const { name } = props
 
-	const url = URLS[name]
+  const url = URLS[name]
 
-	// This should not happen type-wise,
-	// but since this component is used in markdown,
-	// we do need to handle this case.
-	if (url === undefined)
-		throw new Error(`Unknown user: ${name}`)
+  // This should not happen type-wise,
+  // but since this component is used in markdown,
+  // we do need to handle this case.
+  if (url === undefined)
+    throw new Error(`Unknown user: ${name}`)
 
-	return <a target="_blank" href={url}>@{name}</a>
+  return <a target="_blank" href={url}>@{name}</a>
 }
