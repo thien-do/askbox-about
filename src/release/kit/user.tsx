f4d8e846a45e9448ece05adc8eaab853c6063be0
Thien Do <5953369+thien-do@users.noreply.github.com>
2025-04-12 02:12:15 +0700

    fix 3

--- src/release/kit/user.tsx
+++ src/release/kit/user.tsx
@@ -3,10 +3,14 @@ import { ReactElement } from "react"
 const URLS = {
   chinh: "https://www.linkedin.com/in/chinhnq",
   hai: "https://github.com/ng-hai",
-  nhi: "https://github.com/NgocNhi123",
   huy: "https://github.com/huyng12",
+  kha: "https://github.com/nguyenkhatran",
   khoa: "https://github.com/ldakhoa",
-  minh: "https://github.com/monodyle"
+  minh: "https://github.com/monodyle",
+  my: "#",
+  nhi: "https://github.com/NgocNhi123",
+  quang: "https://github.com/iamquang95",
+  tran: "#",
 }
 
 export function ReleaseUser(props: {
